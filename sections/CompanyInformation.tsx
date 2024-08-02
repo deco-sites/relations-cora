74ae7b9d2ad47434aeb2ea93bd0bab28b14caed1
ItsAndre548 <andre548luiz@gmail.com>
2024-08-02 04:35:43 -0300

    efeito zebra adicionado

--- sections/CompanyInformation.tsx
+++ sections/CompanyInformation.tsx
@@ -11,19 +11,23 @@ const formatDate = (dateString) => {
   };
 
 export default function({title, posts}:props) {
+    
     return(
         <div class="px-[110px] py-[126px] flex flex-col justify-center">
             <h1 class="text-5xl text-extrabold py-4">{title}</h1>
             {posts?.map((blog)=>(
-                blog.categories.map((blog2, index)=>(
+                blog.categories.map((blog2)=>(
                     blog2.name === 'investor' && (
-                        <div class={`${index % 2 === 0 ? 'bg-primary-content' : 'bg-secondary'} px-6 py-4 gap-x-20 flex gap-4`}>
+                        <div class="odd:bg-secondary even:bg-primary-content px-6 py-4 gap-x-20 flex gap-4">
                             <p class="text-xl text-primary">{formatDate(blog.date)}</p>
-                            <a href={`/blog/${blog.slug}`} class="text-xl">{blog.title}</a>
+                            <a href={`/blog/${blog.slug}`}class="text-xl">{blog.title}</a>
                         </div>
                     )
-
                     ))))}
         </div>
     )
 }
+
+/*
+even:bg-secondary odd:bg-primary-content
+*/
--- sections/pressBlogList.tsx
+++ sections/pressBlogList.tsx
@@ -17,9 +17,9 @@ export default function({title, posts}:props) {
         <div class="px-[110px] py-[126px] flex flex-col justify-center">
             <h1 class="text-5xl text-extrabold py-4">{title}</h1>
             {posts?.map((test)=>(
-                test.categories.map((test2, index)=>(
+                test.categories.map((test2)=>(
                     test2.name === 'press' && (
-                        <div class={`${index % 2 === 0 ? 'bg-primary-content' : 'bg-secondary'} px-6 py-4 gap-x-20 flex gap-4`}>
+                        <div class="odd:bg-secondary even:bg-primary-content px-6 py-4 gap-x-20 flex gap-4">
                             <p class="text-xl text-primary">{formatDate(test.date)}</p>
                             <a href={`/blog/${test.slug}`} class="text-xl">{test.title}</a>
                         </div>
