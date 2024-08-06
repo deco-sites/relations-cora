8b2f20140c325499bf92c25d0ff7165dc3ed6f9b
ItsAndre548 <andre548luiz@gmail.com>
2024-08-06 17:37:43 -0300

    image with paragraph

--- sections/ImageWithParagraph.tsx
+++ sections/ImageWithParagraph.tsx
@@ -32,8 +32,8 @@ export interface Props {
 }
 
 const PLACEMENT = {
-  left: "flex-col md:flex-row-reverse",
-  right: "flex-col md:flex-row",
+  left: "flex-col lg:flex-row-reverse",
+  right: "flex-col lg:flex-row",
 };
 
 const DEFAULT_IMAGE =
@@ -56,17 +56,17 @@ export default function ImageWithParagraph({
 }: Props) {
   const eng = urlData.lang == 'EN' ? true : false;
   return (
-    <div class="relative px-4 md:px-20 my-4">
-      {imageBackground && <div class="hidden md:block absolute bg-primary-content w-5/12 h-full top-0 right-0 rounded-l-3xl -z-50"/>}
+    <div class="relative px-4 lg:px-20 my-4">
+      {imageBackground && <div class="hidden lg:block absolute bg-primary-content w-5/12 h-full top-0 right-0 rounded-l-3xl -z-50"/>}
       <div class="container mx-auto max-w-[1060px] mx-4 text-sm">
         <div
           class={`flex ${
             PLACEMENT[placement]
-          } gap-12 md:gap-20 text-left items-center z-10 ${
+          } gap-12 lg:gap-20 text-left items-center z-10 ${
             disableSpacing?.top ? "" : "pt-12 lg:pt-28"
           } ${disableSpacing?.bottom ? "" : "pb-12 lg:pb-28"}`}
         >
-          <div class="w-full md:w-1/2 border border-secondary rounded-lg overflow-hidden">
+          <div class="w-full lg:w-1/2 border border-secondary rounded-lg overflow-hidden">
             <Image
               width={640}
               class="object-fit z-10"
@@ -77,14 +77,14 @@ export default function ImageWithParagraph({
               loading="lazy"
             />
           </div>
-          <div class="w-full md:w-1/2 space-y-2 md:space-y-4 md:max-w-xl gap-4 z-10">
+          <div class="w-full lg:w-1/2 space-y-2 lg:space-y-4 lg:max-w-xl gap-4 z-10">
           {tagline && <p class="text-sm font-semibold">
               {tagline}
             </p>}
-            <p class="text-3xl md:text-5xl text-primary font-bold leading-snug">
+            <p class="text-3xl lg:text-5xl text-primary font-bold leading-snug">
               {eng ? englishTitle : portugueseTitle}
             </p>
-            <p class="leading-normal text-xl md:text-2xl">
+            <p class="leading-normal text-xl lg:text-2xl">
               {eng ? englishDescription : portugueseDescription}
             </p>
             <p class="leading-normal text-[22px] pt-4">{eng ? englishSmallDescription : portugueseSmallDescription}</p>
@@ -95,7 +95,7 @@ export default function ImageWithParagraph({
                   id={item?.id}
                   href={item?.href}
                   target={item?.href.includes("http") ? "_blank" : "_self"}
-                  class={`text-2xl px-12 py-2 md:py-4 border rounded-full
+                  class={`text-2xl px-12 py-2 lg:py-4 border rounded-full
                     ${!item.style || item.style == "Outline" && "btn-outline"}
                     ${item.style == "Ghost" && "btn-ghost"}
                   `}
