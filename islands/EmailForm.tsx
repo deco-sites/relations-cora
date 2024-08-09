389942c1d7746e349aa871e131536a453061b958
ItsAndre548 <andre548luiz@gmail.com>
2024-08-08 21:15:17 -0300

    mudando a cor do texto no emailsbuscribe

--- islands/EmailForm.tsx
+++ islands/EmailForm.tsx
@@ -26,7 +26,7 @@ export default function EmailForm({ eng, englishButtonText, portugueseButtonText
     } 
     return (
         <form class="flex flex-column flex-wrap justify-center gap-3 align-center pb-[56px] lg:flex-row" onSubmit={handleSubmit}>
-        <input type="email" placeholder="E-mail" class="lg:px-12 px-4 py-2 text-primary text-[24px] rounded-xl" onChange={handleChange} />
+        <input type="email" placeholder="E-mail" class="lg:px-12 px-4 py-2 text-base-200 text-[24px] rounded-xl" onChange={handleChange} />
         <button type="submit" class="bg-primary py-2 px-4 text-[24px] text-secondary lg:px-[55px] lg:py-[10px] rounded-xl">{eng ? englishButtonText : portugueseButtonText}</button>
     </form>
     )
